2bd5817aa5c4ca4cfa3d062a88d79b5f4ef68286
Sergey Alemasov <alemasovsa@sibedge.com>
2019-08-11 11:29:28 +0400

    watched several lectures

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] float missileSpeed = 10f;
+
+    Rigidbody2D selfRigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selfRigidBody2D = GetComponent<Rigidbody2D>();
+        SetInitialSpeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetInitialSpeed()
+    {
+        selfRigidBody2D.velocity = new Vector2(0, missileSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "MissileCollector")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeedX = 10f;
+    [SerializeField] float moveSpeedY = 5f;
+    [SerializeField] GameObject laserPrefab = null;
+
+    // state
+    Vector3 minCoords;
+    Vector3 maxCoords;
+    Vector3 selfSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupMoveBoundaries();
+    }
+
+    void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        minCoords = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        maxCoords = gameCamera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        selfSize = GetComponent<Renderer>().bounds.size;
+
+        float selfHalfX = selfSize.x / 2;
+        float selfHalfY = selfSize.y / 2;
+
+        minCoords.x += selfHalfX;
+        maxCoords.x -= selfHalfX;
+
+        minCoords.y += selfHalfY;
+        maxCoords.y -= selfHalfY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * moveSpeedX;
+        var deltaY = Input.GetAxis("Vertical") * moveSpeedY;
+
+        deltaX *= Time.deltaTime;
+        deltaY *= Time.deltaTime;
+
+        float newPosX = Mathf.Clamp(transform.position.x + deltaX, minCoords.x, maxCoords.x);
+        float newPosY = Mathf.Clamp(transform.position.y + deltaY, minCoords.y, maxCoords.y);
+
+        transform.position = new Vector2(newPosX, newPosY);
+    }
+
+    void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
